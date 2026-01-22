c07dc4d289ee04375f0f455d91ca4fc51fca5401
07karthik0704-afk <07karthik.07.04@gmail.com>
2026-01-22 11:15:06 +0530

    updated

--- day5/app/models/product.rb
+++ day5/app/models/product.rb
@@ -1,5 +1,16 @@
 class Product < ApplicationRecord
+#   validates :stock, numericality: true
+#   validates :stock, numericality: { greater_than_or_equal_to: 10, less_than_or_equal_to: 20 }
+#   validates :description, length: { maximum: 500 }
 
-    #arr = Product.all.limit(10)
-   
+validates :price, numericality:{greater_than_or_equal_to:3}
+validate :check_price
+
+  def check_price
+    if stock.to_i == 0 && price.to_f <= 0
+      errors.add(:stock, "Stock is not available...")
+    end
+  end
 end
+
+
