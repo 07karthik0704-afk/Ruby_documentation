20195358c367b6ab42429a463a20d500f7b800e3
07karthik0704-afk <07karthik.07.04@gmail.com>
2026-02-05 00:27:52 +0530

    updated

--- day1/db/schema.rb
+++ day1/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_02_041703) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_03_090436) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -68,6 +68,13 @@ ActiveRecord::Schema[8.1].define(version: 2026_02_02_041703) do
     t.string "name"
     t.integer "phone_number"
     t.integer "rating"
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "count"
+    t.datetime "created_at", null: false
+    t.string "details"
     t.datetime "updated_at", null: false
   end
 
