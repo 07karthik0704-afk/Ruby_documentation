de61765eb35515ee997eaff24447bfc41fef7f00
07karthik0704-afk <07karthik.07.04@gmail.com>
2026-02-05 21:28:12 +0530

    updated

---
+++ day17/HomeWork.rb
@@ -0,0 +1,55 @@
+# Task -1 
+# Reverse the string 
+
+# name="karthik"
+
+# name=name.reverse
+
+# puts name
+
+# -------------------------------------------------------------------------
+
+# Task -2
+
+#This also same but using custom input 
+
+
+# name=gets.chomp
+#  name=name.reverse
+
+# puts name
+
+# -------------------------------------------------------------------------
+
+# Task-3
+
+# Check whether the given is pallindrome or not
+
+# name=gets.chomp
+
+# rev_name=name.reverse
+
+# if (name==rev_name)
+# 	puts "pallindrome"
+# else
+# 	puts "Not a pallindrome"
+# end
+
+# -------------------------------------------------------------------------
+
+# Task-4
+
+# Factorial of a given number
+
+# num=gets.chomp.to_i
+# fact =1 
+# i=1
+# while (num>i)
+# 	fact=fact*num
+# 	num=num-1
+# end
+# puts "The factorial  is #{fact}" 
+
+
+
+
